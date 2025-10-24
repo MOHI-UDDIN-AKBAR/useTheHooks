aeac9f256f3d6eca15d31d82eb88d4b9cecc58b5
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2025-10-24 12:25:29 +0200

    feat: update App component to include UseEffectHook and modify return structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./global.css";
+// import UseStateHook from "./hooks/useStateHook/UseStateHook";
+import UseEffectHook from "./hooks/useEffectHook/UseEffectHook";
 
 const App = () => {
-  return <>Hello World</>;
+  return (
+    <>
+      {/* <UseStateHook /> */}
+      <UseEffectHook />
+    </>
+  );
 };
 
 export default App;
