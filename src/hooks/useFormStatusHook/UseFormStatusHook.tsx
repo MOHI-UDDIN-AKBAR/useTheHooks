886eb3d806743a8616e97ee81092a40c645646c3
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2025-10-29 19:38:57 +0100

    feat: refactor form to use react-dom's useFormStatus hook

--- src/hooks/useFormStatusHook/UseFormStatusHook.tsx
+++ src/hooks/useFormStatusHook/UseFormStatusHook.tsx
@@ -1,69 +1,73 @@
-import { type FormEvent, useState } from 'react';
+import { useState } from 'react';
+import { useFormStatus } from 'react-dom';
 
 type Item = {
 	id: string;
 	value: string;
 };
 
+function createItem(value: string): Promise<Item> {
+	const newItem = { id: crypto.randomUUID(), value };
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(newItem);
+		}, 2000);
+	});
+}
+
+const Button: React.FC = () => {
+	const { pending } = useFormStatus();
+	return (
+		<button
+			type="submit"
+			className="btn"
+			disabled={pending}
+			style={{
+				borderColor: pending ? '#949494' : '#877460',
+			}}
+		>
+			{pending ? 'Creating...' : 'Create'}
+		</button>
+	);
+};
 const UseFormStatusHook: React.FC = () => {
 	const [items, setItems] = useState<Item[]>([]);
 	const [inputValue, setInputValue] = useState('');
-	const [isPending, setIsPending] = useState(false);
 
-	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		if (!inputValue.trim()) return;
-		setIsPending(true);
-		const newItem = (await createItem(inputValue)) as Item;
+	const handleSubmit = async (formData: FormData) => {
+		const { item } = Object.fromEntries(formData.entries()) as { item: string };
+		if (!item.trim()) return;
+
+		const newItem = await createItem(item);
 		setItems((prev) => [...prev, newItem]);
-		setIsPending(false);
 		setInputValue('');
 	};
 	return (
 		<>
-			<form onSubmit={handleSubmit}>
+			<form action={handleSubmit}>
 				<div className="input-group">
-					<label htmlFor="title">
+					<label htmlFor="item">
 						Title :{' '}
 						<input
 							type="text"
-							name="title"
+							name="item"
 							id="title"
 							value={inputValue}
 							onChange={(e) => setInputValue(e.target.value)}
 						/>
 					</label>
 				</div>
-				<button
-					type="submit"
-					className="btn"
-					disabled={isPending}
-					style={{
-						borderColor: isPending ? '#949494' : '#877460',
-					}}
-				>
-					{isPending ? 'Creating...' : 'Create'}
-				</button>
+				<Button />
 			</form>
 			<div className="items">
-				{items.length
-					? items.map(({ id, value }) => <div key={id}> {value}</div>)
-					: null}
+				{items.length > 0 ? (
+					items.map(({ id, value }) => <div key={id}>{value}</div>)
+				) : (
+					<p>No Items yet</p>
+				)}
 			</div>
 		</>
 	);
 };
 
 export default UseFormStatusHook;
-
-function createItem(value: string) {
-	return wait({ id: crypto.randomUUID(), value }, 2000);
-}
-
-function wait(newTodo: { id: string; value: string }, duration: number) {
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			resolve(newTodo);
-		}, duration);
-	});
-}
