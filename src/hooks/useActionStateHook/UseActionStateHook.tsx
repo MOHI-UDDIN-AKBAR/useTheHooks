2f1f92ecc259cca0cc1661d5be5028203398974a
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2026-02-15 11:49:58 +0100

    style(use-action-state): normalize quotes and format file

--- src/hooks/useActionStateHook/UseActionStateHook.tsx
+++ src/hooks/useActionStateHook/UseActionStateHook.tsx
@@ -1,49 +1,49 @@
-import { useActionState } from 'react';
+import { useActionState } from "react";
 
 type DataType = { message?: string; error?: string };
 
 async function saveData(
-	prevState: unknown,
-	formData: FormData,
+  prevState: unknown,
+  formData: FormData,
 ): Promise<DataType> {
-	await wait(3000);
-	const { firstName } = Object.fromEntries(formData.entries()) as {
-		firstName: string;
-	};
+  await wait(3000);
+  const { firstName } = Object.fromEntries(formData.entries()) as {
+    firstName: string;
+  };
 
-	formData.set('firstName', '');
+  formData.set("firstName", "");
 
-	if (!firstName.trim()) {
-		return Promise.resolve({ error: 'Value is empty' });
-	}
-	return Promise.resolve({ message: 'Value is correct' });
+  if (!firstName.trim()) {
+    return Promise.resolve({ error: "Value is empty" });
+  }
+  return Promise.resolve({ message: "Value is correct" });
 }
 
 function wait(duration: number): Promise<unknown> {
-	return new Promise((resolve) => setTimeout(resolve, duration));
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
 const UseActionStateHook: React.FC = () => {
-	const [data, action, isPending] = useActionState(saveData, undefined);
+  const [data, action, isPending] = useActionState(saveData, undefined);
 
-	return (
-		<>
-			<form action={action}>
-				<div className="input-group">
-					<label htmlFor="firstName">
-						FirstName : <input type="text" name="firstName" id="firstName" />
-					</label>
-				</div>
-				<button type="submit" disabled={isPending}>
-					Submit
-				</button>
-			</form>
-			<div>
-				{data?.message && <p style={{ color: 'green' }}>{data?.message}</p>}
-				{data?.error && <p style={{ color: 'red' }}>{data?.error}</p>}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <form action={action}>
+        <div className="input-group">
+          <label htmlFor="firstName">
+            FirstName : <input type="text" name="firstName" id="firstName" />
+          </label>
+        </div>
+        <button type="submit" disabled={isPending}>
+          Submit
+        </button>
+      </form>
+      <div>
+        {data?.message && <p style={{ color: "green" }}>{data?.message}</p>}
+        {data?.error && <p style={{ color: "red" }}>{data?.error}</p>}
+      </div>
+    </>
+  );
 };
 
 export default UseActionStateHook;
